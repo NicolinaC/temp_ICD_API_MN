267e04c11792e07ecc742e90c9b0f42f806f6d85
nicolinacap25 <nicolina.capitanio@stonybrook,edu>
2023-05-12 01:42:16 +0000

    this is the working file

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
-from flask import Flask
-import pandas as pd
+from flask import Flask, request
+import pandas as pd 
 
-df = pd.read_csv('./data/smalldiagnoses2018.csv')
+df = pd.read_csv('./data/services2019.csv')
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def home():
-    return 'this is a API service for MN ICD code details'  
+    return 'this is a API service for MN Service code details'
 
 @app.route('/preview', methods=["GET"])
 def preview():
-    top10rows = df.head(1)
+    top10rows = df.head(10)
     result = top10rows.to_json(orient="records")
     return result
 
-@app.route('/icd/<value>', methods=['GET'])
-def icdcode(value):
+@app.route('/sex/<value>', methods=['GET'])
+def sex(value):
     print('value: ', value)
-    filtered = df[df['principal_diagnosis_code'] == value]
-    return filtered.to_json(orient="records")
+    filtered = df[df['sex'] == value]
+    if len(filtered) <= 0:
+        return 'There is nothing here'
+    else: 
+        return filtered.to_json(orient="records")
 
-@app.route('/icd/<value>/sex/<value2>')
-def icdcode2(value, value2):
-    filtered = df[df['principal_diagnosis_code'] == value]
-    filtered2 = filtered[filtered['sex'] == value2]
+@app.route('/sex/<value>/payer/<value2>')
+def sex2(value, value2):
+    filtered = df[df['sex'] == value]
+    filtered2 = filtered[filtered['payer'] == value2]
     if len(filtered2) <= 0:
         return 'There is nothing here'
     else: 
-        return filtered2.to_json(orient="records")   
-        
+        return filtered2.to_json(orient="records") 
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+    
+    
